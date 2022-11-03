5001b7d6d13e23e0e4e0b4e4f7f3cdcfe6cadd2c
JuanCruzC97 <juancruzcamacho97@gmail.com>
2022-11-03 16:41:12 -0300

    Creamos modulos

---
+++ logistica/componentes/camion.py
@@ -0,0 +1,234 @@
+
+class Camion(object):
+    """ 
+    La clase Camión permite generar instancias de camiones con sus respectivas características:
+        - ix: Identificador del camión.
+        - carga_max: Máxima carga admitida.
+        - pedidos_max: Máximo número de pedidos admitidos.
+        - dist_max: Máxima distancia entre clientes.
+        
+    Dentro de pedidos_asignados se guardan todos los pedidos que se agregan al camión acompañados del ix del pedido.
+    Se puede acceder fácilmente a la carga total actual del camion y cantidad de pedidos con esos atributos.
+    """
+    
+    def __init__(self, ix, carga_max, pedidos_max, dist_max):
+        self.ix = ix
+        self.carga_max = carga_max
+        self.pedidos_max = pedidos_max
+        self.dist_max = dist_max
+        self.pedidos_asignados = {}
+        self.carga_total = 0
+        self.cantidad_pedidos = 0
+        
+        
+    def __str__(self):
+        return f'Camión {self.ix}\nCarga Total {self.carga_total} tn\nPedidos {[ped.ix for ped in self.pedidos_asignados.values()]}\nCosto Total {self.get_costo()}\nCosto por tn {self.get_costo_tn()}'
+    
+    def __repr__(self):
+        return f'Camión {self.ix}\nCarga Total {self.carga_total} tn\nPedidos {[ped.ix for ped in self.pedidos_asignados.values()]}\nCosto Total {self.get_costo()}\nCosto por tn {self.get_costo_tn()}'
+    
+    def _check_pedido_carga(self, pedido):
+        """ 
+        Permite pasarle al camión un nuevo pedido y revisar carga máxima.
+            - Devuelve True si sumando el nuevo pedido no excedemos la carga máxima.
+            - Devuelve False si sumando el nuevo pedido excedemos la carga máxima.
+        """
+        return (self.carga_total + pedido.carga) <= self.carga_max
+    
+    def _check_pedido_cantidad(self):
+        """ 
+        Permite revisar si se alcanzó la cantidad de pedidos máxima.
+            - Devuelve True si sumando el nuevo pedido no excedemos la cantidad de pedidos máxima.
+            - Devuelve False si sumando el nuevo pedido excedemos la cantidad de pedidos máxima.
+        """
+        return self.cantidad_pedidos < self.pedidos_max
+    
+    def _check_pedido_distancia(self, pedido):
+        """ 
+        Permite pasarle al camión un nuevo pedido y revisar que su distancia no supere la distancia máxima
+        con el resto de pedidos.
+            - Devuelve True si sumando el nuevo pedido no excedemos la distancia máxima para ningún pedido.
+            - Devuelve False si sumando el nuevo pedido excedemos la distancia máxima en al menos un pedido.
+        """
+        dist_check = [pedido.distancia(other) <= self.dist_max for other in self.pedidos_asignados.values()]
+        return all(dist_check)
+    
+    def check_nuevo_pedido(self, pedido):
+        """ 
+        Permite pasarle al camión un nuevo pedido y revisar si puede agregarse al camión cumpliendo
+        la carga máxima, distancia máxima y cantidad de pedidos máxima.
+            - Devuelve True si sumando el nuevo pedido no excedemos ninguna restricción.
+            - Devuelve False si sumando el nuevo pedido excedemos alguna restricción.
+        """
+        #return not pedido.asignado and self._check_pedido_carga(pedido) and self._check_pedido_cantidad(pedido) and self._check_pedido_distancia(pedido)
+        return self._check_pedido_carga(pedido) and self._check_pedido_cantidad() and self._check_pedido_distancia(pedido)
+        
+    def check_intercambio_pedido(self, pedido):
+        """ 
+        Permite pasarle al camión un pedido que no pertenezca al mismo y revisar a 
+        que pedidos ya asignados podría reemplazar. Esta comparación se realiza con el pedido que se pasa al método
+        contra todos los pedidos asignados en el camión.
+            
+        Que un nuevo pedido pueda reemplazar a otro implica que:
+            - Eliminando la carga del pedido original (a reemplazar) y agregando la nueva carga no se supera la carga máxima.
+            - La distancia del nuevo pedido con los pedidos restantes del camión (sin tener en cuenta el pedido a reemplazar) no supera la distancia máxima.
+            
+        Returns:
+            list: Lista de ix de pedidos que podrían ser reemplazados por el pedido pasado como argumento del método.
+        """    
+        
+        # Revisamos que el pedido no esté asignado previamente al camión.
+        if pedido.ix not in self.get_ix_pedidos():
+            
+            # Creamos una lista de ixs reemplazables por el pedido nuevo.
+            ix_pedidos_reemplazables = []
+            
+            # Para cada uno de los pedidos ya asignado al camión se hace el chequeo.
+            for pedido_original in self.get_pedidos():
+                # Calculamos la carga nueva que tendría el camión con el reemplazo de pedidos.
+                nueva_carga = self.carga_total + pedido.carga - pedido_original.carga
+                # Generamos una lista de bool revisando si el nuevo pedido incorporado supera la distancia máxima con los pedidos restantes en el camión.
+                distancias = [pedido.distancia(pedido_restante) <= self.dist_max for pedido_restante in self.get_pedidos() if pedido_restante.ix != pedido_original.ix]
+                
+                # Si la nueva carga no supera la carga máxima y las distancias con pedidos restantes son todas menores al máximo
+                # agregamos el ix del pedido a reemplazar en la lista.
+                if nueva_carga <= self.carga_max and all(distancias):
+                    ix_pedidos_reemplazables.append(pedido_original.ix)
+                    
+            return ix_pedidos_reemplazables
+        
+        # Si el pedido ya estaba asignado al camión se devuelve una lista vacía.
+        else:
+            return []
+        
+        
+    def add_pedido(self, pedido):
+        """
+        Permite pasarle al camión un pedido no asignado previamente e incorporarlo al mismo.
+        Este método no realiza los chequeos de las restricciones del camión.
+        """
+        if not pedido.asignado:
+            # Se agrega el pedido al diccionario de pedidos asignados de este camión.
+            self.pedidos_asignados[pedido.ix] = pedido
+            # Se actualiza la carga total y cantidad de pedidos totales.
+            self.carga_total += pedido.carga
+            self.cantidad_pedidos += 1
+            # Se actualizan las propiedades del pedido agregado.
+            pedido.asignado = True
+            pedido.camion_ix = self.ix
+        #     return True
+        # else:
+        #     return False
+            
+    def add_pedido_checked(self, pedido):
+        """
+        Permite pasarle al camión un pedido no asignado previamente e incorporarlo al mismo.
+        A diferencia del método add_pedido() este método sólo agrega el pedido si pasa los chequeos de las restricciones.
+        """
+        if self.check_nuevo_pedido(pedido):
+            self.add_pedido(pedido)
+        
+    
+    def remove_pedido(self, pedido_ix):
+        """
+        Permite pasarle al camión un ix de un pedido asignado y eliminarlo del diccionario
+        de pedidos asignados.
+        """
+        # Confirma que el pedido esté asignado a este camión.
+        if pedido_ix in self.get_ix_pedidos():
+            # Actualiza los parámetros del pedido.
+            self.get_pedido(pedido_ix).asignado = False
+            self.get_pedido(pedido_ix).camion_ix = None
+            # Actualiza los parámetros del camión.
+            self.carga_total -= self.pedidos_asignados.get(pedido_ix).carga
+            self.cantidad_pedidos -= 1
+            # Elimina el pedido.
+            self.pedidos_asignados.pop(pedido_ix)
+        else:
+            print(f"El pedido {pedido_ix} no está en el camión {self.ix}")    
+
+    def get_ix_pedidos(self):
+        """
+        Returns:
+            list: Lista de ix de pedidos asignados al camión.
+        """        
+        return list(self.pedidos_asignados.keys())
+    
+    def get_pedidos(self):
+        """
+        Returns:
+            list: Lista de pedidos asignados al camión.
+        """     
+        return list(self.pedidos_asignados.values())
+    
+    def get_pedido(self, ix):
+        """
+        Args:
+            ix (int or str): Código identificador del pedido que queremos obtener de los pedidos asignados al camión.
+
+        Returns:
+            Pedido: Pedido ix asignado al camión.
+        """
+        return self.pedidos_asignados.get(ix)
+    
+    def get_ix(self):
+        """
+        Returns:
+            int or str: Identificador ix del camión.
+        """
+        return self.ix
+    
+    def get_carga_total(self):
+        """
+        Returns:
+            int: Carga total de pedidos asignados del camión.
+        """       
+        return self.carga_total
+    
+    def count_pedidos(self):
+        """
+        Returns:
+            int: Cantidad de pedidos asignados del camión.
+        """          
+        return self.cantidad_pedidos
+    
+    def get_costo(self):
+        """
+        Returns:
+            int: Costo del camión en función de la carga total.
+        """   
+         
+        if self.carga_total == 0:
+            costo = 5000
+        elif self.carga_total <= 4:
+            costo = 5600
+        elif self.carga_total > 4 and self.carga_total < 6.5:
+            costo = 1400*self.carga_total
+        elif self.carga_total >= 6.5 and self.carga_total < 9.5:
+            costo = 1200*self.carga_total
+        else:
+            costo = 1000*self.carga_total
+                
+        return costo 
+    
+    def get_costo_tn(self):
+        """
+        Returns:
+            float: Costo por tn del camión en función de la carga total.
+        """   
+        if self.carga_total != 0:
+            return self.get_costo()/self.carga_total
+        else:
+            return None
+
+    def reset_pedidos(self):
+        """
+        Elimina todos los pedidos asignados del camión.
+        """
+        self.pedidos_asignados.clear()
+        self.carga_total = 0
+        self.cantidad_pedidos = 0
+        
+        
+        
+        
---
+++ logistica/componentes/ruteo.py
@@ -0,0 +1,341 @@
+import random
+from .camion import Camion
+from .pedido import Pedido
+
+class Ruteo(object):
+    """ 
+    La clase Ruteo contiene toda la información de camiones disponibles y pedidos requeridos:
+        - camiones: Diccionario de camiones disponibles identificados por su ix.
+        - pedidos: Diccionario de pedidos disponibles identificados por su ix.
+        - costo de oportunidad: Costo de pedidos no asignados en $/tn.
+        - presupuesto: Presupuesto previsto en $/tn.
+        - random_state: Permite definir la semilla para la generación de valores aleatorios.
+    """
+    
+    def __init__(self, df_camiones, df_pedidos, costo_oportunidad, presupuesto, random_state):
+        self.camiones = self._load_camiones(df_camiones)
+        self.pedidos = self._load_pedidos(df_pedidos)
+        self.costo_oportunidad = costo_oportunidad
+        self.presupuesto = presupuesto
+        
+        # Uso el random state para determinar la generación de solución inicial.
+        self.random_state = random_state
+        random.seed(self.random_state)
+        
+    def _load_camiones(self, df_camiones):
+        """
+        Este método genera el diccionario de camiones a partir de un DataFrame.
+
+        Args:
+            df_camiones (pd.DataFrame): Dataframe que contiene la información de camiones.
+                                        El mismo debe contener las siguientes columnas:
+                                        - camion: Contiene el identificador ix del camión.
+                                        - carga_max: Carga máxima admitida para cada camión.
+                                        - pedidos_max: Cantidad de pedidos máximo admitido para cada camión.
+                                        - dist_max: Distancia máxima entre pedidos asignados a un camión.
+
+        Returns:
+            dict: Diccionario con los camiones del ruteo.
+        """
+        dict_camiones = {row.camion:Camion(ix=row.camion, carga_max=row.carga_max, pedidos_max=row.pedidos_max, dist_max=row.dist_max) for _, row in df_camiones.iterrows()}
+        return dict_camiones
+
+    def _load_pedidos(self, df_pedidos):
+        """
+        Este método genera el diccionario de pedidos a partir de un DataFrame.
+
+        Args:
+            df_pedidos (pd.DataFrame): Dataframe que contiene la información de pedidos.
+                                        El mismo debe contener las siguientes columnas:
+                                        - cliente: Contiene el identificador ix del pedido.
+                                        - pedidos: Carga de cada pedido.
+                                        - coord_x: Coordenada x del cliente.
+                                        - coord_y: Coordenada y del cliente.
+
+        Returns:
+            dict: Diccionario con los pedidos del ruteo.
+        """
+        dict_pedidos = {row.cliente:Pedido(ix=row.cliente, x=row.coord_x, y=row.coord_y, carga=row.pedidos) for _, row in df_pedidos.iterrows() if row.pedidos != 0}
+        return dict_pedidos
+    
+    def get_ix_camiones(self):
+        """
+        Returns:
+            list: Lista de ix de camiones.
+        """   
+        return list(self.camiones.keys())
+    
+    def get_ix_pedidos(self):
+        """
+        Returns:
+            list: Lista de ix de pedidos.
+        """  
+        return list(self.pedidos.keys())
+    
+    def get_camiones(self):
+        """
+        Returns:
+            list: Lista de camiones.
+        """  
+        return list(self.camiones.values())
+    
+    def get_pedidos(self):
+        """
+        Returns:
+            list: Lista de pedidos.
+        """          
+        return list(self.pedidos.values())
+    
+    def get_camion(self, ix):
+        """
+        Returns:
+            Camion: Camion con identificador ix.
+        """  
+        return self.camiones.get(ix)
+    
+    def get_pedido(self, ix):
+        """
+        Returns:
+            Pedido: Pedido con identificador ix.
+        """  
+        return self.pedidos.get(ix)
+    
+    def count_camiones(self):
+        """
+        Returns:
+            int: Cantidad de camiones.
+        """  
+        return len(self.camiones)
+    
+    def count_pedidos(self):
+        """
+        Returns:
+            int: Cantidad de pedidos.
+        """  
+        return len(self.pedidos)
+    
+        
+    def get_solucion_inicial(self, mode):
+        """
+        Permite generar una solución inicial, es decir, realizar una asignación inicial de pedidos en camiones.
+        
+        Permite usar varios modos diferentes de generación de solución inicial con el argumento de este método.
+
+        Args:
+            mode (_type_): _description_
+        """
+        
+        if mode == "simple":
+            self._get_solucion_inicial_simple()
+                    
+        elif mode == "random":
+            self._get_solucion_inicial_random()     
+            
+        else:
+            self._get_solucion_inicial_simple()
+    
+    # EXPLICAR Y CHEQUEAR BIEN LAS SOLUCIONES INICIALES.              
+    
+    def _get_solucion_inicial_simple(self):
+        """
+        Genera una solución inicial deterministica. 
+        Para cada camión, toma cada pedido disponible y lo intenta asignar, siendo asignado si pasa los chequeos.
+        Queda definido por el orden de carga de camiones y pedidos.
+        """
+        for camion in self.get_camiones():
+            for pedido in self.get_pedidos():
+                camion.add_pedido_checked(pedido)
+                
+                
+    def _get_solucion_inicial_random(self):
+        """
+        Genera una solución con aleatoriedad.
+            1. Mezcla los ix de pedidos de manera aleatoria.
+            2. Para cada pedido en el orden aleatorio genera una lista de ix de camiones aleatoria
+            3. Para cada camión en orden aleatorio se intenta agregar el pedido. En caso de lograrse
+               pasa al siguiente pedido en orden aleatorio volviendo a 2.
+            
+        Prueba introducir todos los pedidos en todos los camiones en orden aleatorio.
+        """
+        # Identificadores aleatorios de pedidos.
+        ix_pedidos_rnd = random.sample(self.get_ix_pedidos(), self.count_pedidos())
+        
+        for ix_pedido in ix_pedidos_rnd:
+            pedido = self.get_pedido(ix_pedido)
+            ix_camiones_rnd = random.sample(self.get_ix_camiones(), self.count_camiones())
+            
+            for ix_camion in ix_camiones_rnd:
+                self.get_camion(ix_camion).add_pedido_checked(pedido)
+                
+                if pedido.asignado:
+                    break
+    
+    
+    def generar_vecino(self, prob=1):
+        """
+        Realiza una modificación en la instancia de la solución, creando una nueva solución similar y válida de ruteo.
+        
+            1. Se selecciona un pedido al azar entre todos los pedidos. Este es el pedido a modificar (pedido_mod).
+            2. Chequeamos si el pedido_mod puede ingresar a un camión de manera directa.
+            
+            3. Si el pedido ya está asignado:
+                a. Obtenemos el ix del camión al que estaba asignado (camion al que le iría el nuevo pedido en caso de reemplazo).
+                b. Eliminamos el pedido_mod del camión.
+                
+                4. Si el pedido_mod entra directamente en otro camion y prob es mayor al valor aleatorio uniforme(0,1):
+                   Con prob = 1 (default) el 100% de las veces que entre directamente irá a esos camiones.
+                    a. Se lo asigna directamente a alguno de los camiones directos al azar.
+                       Si entra de manera directa no hace falta chequear que entre. 
+                       No hace falta intercambiar por otro pedido porque entra directo.
+                      
+                4. Si el pedido no entra directamente en otro camion:
+                    a. Se genera una lista de ix de pedidos que podrían ser reemplazados en sus camiones por pedido_mod.
+                    b. Se genera una lista de ix de pedidos que podrían ser reemplazados y además que pueden entrar en 
+                       el camión en el que estaba pedido_mod.
+                    
+                    5. Si hay al menos 1 pedido reemplazable posible:
+                        a. Se toma un pedido al azar de estos pedidos reemplazables posibles.
+                        b. Se elimina al pedido_reemplazo de su camion.
+                        c. Se asigna el pedido_mod al camión del pedido_reemplazo.
+                        d. Se asigna el pedido_reemplazo en el camión de pedido_mod.
+                        
+                    5. Si no hay al menos 1 pedido reemplazable posible:
+                        a. Se devuelve pedido_mod a su camión original.
+                        
+            3. Si el pedido no está asignado:
+            
+                4. Si el pedido_mod entra directamente en otro camion:
+                    a. Se lo asigna directamente a alguno de los camiones directos al azar.
+                       Si entra de manera directa no hace falta chequear que entre.
+                       No hace falta intercambiar por otro pedido porque entra directo.
+                       
+                4. Si el pedido no entra directamente en otro camion:
+                    a. Se genera una lista de ix de pedidos que podrían ser reemplazados en sus camiones por pedido_mod.
+
+                    5. Si hay al menos 1 pedido reemplazable:
+                        a. Se toma un pedido al azar de estos pedidos reemplazables.
+                        b. Se elimina al pedido_reemplazo de su camion.
+                        c. Se asigna el pedido_mod al camión del pedido_reemplazo. El pedido_reemplazo queda sin asignar.
+
+        """
+        
+        ix_pedido_mod = random.choice(self.get_ix_pedidos())
+        pedido_mod = self.get_pedido(ix_pedido_mod)
+        
+        # Índices de los camiones en los que podría entrar directamente.
+        ix_camion_directo = [camion.ix for camion in self.get_camiones() if camion.check_nuevo_pedido(pedido_mod)]
+        
+        # Si el pedido_mod ya está seleccionado:
+        if pedido_mod.asignado:
+            
+            # Obtenemos el ix del camion al que pertenece pedido_mod
+            ix_camion_mod = pedido_mod.camion_ix
+            # Eliminamos de su camión a pedido_mod.
+            self.get_camion(ix_camion_mod).remove_pedido(pedido_mod.ix)
+            
+            # Si el pedido_mod entra de manera directa en otro camión, es asignado de manera directa a alguno de esos camiones al azar.
+            if len(ix_camion_directo) > 0 and prob >= random.uniform(0,1):
+                
+                # Se elige un camión directo para cambiar 
+                ix_camion_new = random.choice(ix_camion_directo)
+                # No hace falta chequear de que entre porque fue revisado previamente.
+                self.get_camion(ix_camion_new).add_pedido(pedido_mod)
+                
+            # Si el pedido no entra directamente en un camión debemos reemplazar pedidos.
+            else:
+                
+                ix_pedidos_reemplazables = []
+                
+                # Obtengo todos los ix de pedidos que podrían ser reemplazados por pedido_mod en sus camiones.
+                for camion in self.get_camiones():
+                    ix_pedidos_reemplazables += camion.check_intercambio_pedido(pedido_mod)
+                               
+                ix_pedidos_reemplazables_posibles = []
+                
+                # Para que sea posible el intercambio tenemos que quedarnos con los ix de pedidos
+                # que entrarían en el camión donde estaba pedido_mod.                
+                for ix_pedido_reemplazable in ix_pedidos_reemplazables:
+                    pedido_reemplazable = self.get_pedido(ix_pedido_reemplazable)
+                    if self.get_camion(ix_camion_mod).check_nuevo_pedido(pedido_reemplazable):
+                        ix_pedidos_reemplazables_posibles += ix_pedido_reemplazable
+                        
+                # Si tengo al menos un reemplazo posible.
+                if len(ix_pedidos_reemplazables_posibles) > 0:
+                    pedido_reemplazo = self.get_pedido(random.choice(ix_pedidos_reemplazables_posibles))
+                    ix_camion_new = pedido_reemplazo.camion_ix
+                    self.get_camion(ix_camion_new).remove_pedido(pedido_reemplazo.ix)
+                    
+                    self.get_camion(ix_camion_new).add_pedido(pedido_mod)
+                    self.get_camion(ix_camion_mod).add_pedido(pedido_reemplazo)
+                    
+                # Si no tengo ningún reemplazo posible reasigno pedido_mod a su camión original.
+                else:
+                    self.get_camion(ix_camion_mod).add_pedido_checked(pedido_mod)
+
+        # Si el pedido no está asignado:
+        else:
+            
+            # Si el pedido_mod entra de manera directa en otro camión, es asignado de manera directa a alguno de esos camiones al azar.
+            if len(ix_camion_directo) > 0:
+                
+                # Se elige un camión directo para cambiar 
+                ix_camion_new = random.choice(ix_camion_directo)
+                # No hace falta chequear de que entre porque fue revisado previamente.
+                self.get_camion(ix_camion_new).add_pedido(pedido_mod)
+                
+            # Si el pedido no entra directamente en un camión debemos reemplazar pedidos.   
+            else:
+                
+                ix_pedidos_reemplazables = []
+                
+                # Obtengo todos los ix de pedidos que podrían ser reemplazados por pedido_mod en sus camiones.
+                for camion in self.get_camiones():
+                    ix_pedidos_reemplazables += camion.check_intercambio_pedido(pedido_mod)
+
+                # QUE PASA SI NO TENGO NINGUNO
+                if len(ix_pedidos_reemplazables) > 0:
+                    pedido_reemplazo = self.get_pedido(random.choice(ix_pedidos_reemplazables))
+                    ix_camion_new = pedido_reemplazo.camion_ix
+                    self.get_camion(ix_camion_new).remove_pedido(pedido_reemplazo.ix)
+                    
+                    self.get_camion(ix_camion_new).add_pedido(pedido_mod)
+             
+    
+    # PARA CAMBIAR TODA ESTA PARTE
+    # GENERAR UN DATAFRAME CON TODOS LOS DATOS.
+    def _set_results(self):
+        self._set_carga_total()
+        self._set_costo_camiones()
+        self._set_costo_no_asignados()
+        self._set_costo_total()
+        self._set_costo_total_tn()
+        self._set_ahorro()
+        
+    def get_results(self):
+        self._set_results()
+        print("--Resultados--")
+        print(f"Carga Total {self.carga_total} tn")
+        print(f"Costo Camiones {self.costo_camiones} $")
+        print(f"Costo Oportunidad {self.costo_no_asignados} $")
+        print(f"Costo Total {self.costo_total} $")
+        print(f"Costo Total por tn {self.costo_total_tn} $")
+        print(f"Ahorro {self.ahorro*100}%")
+    
+    def _set_carga_total(self):
+        self.carga_total = sum([camion.get_carga_total() for camion in self.camiones.values()])
+        
+    def _set_costo_camiones(self):
+        self.costo_camiones = sum([camion.get_costo() for camion in self.camiones.values()])
+        
+    def _set_costo_no_asignados(self):
+        carga_no_asignada = sum([pedido.get_carga() for pedido in self.pedidos.values() if not pedido.asignado])
+        self.costo_no_asignados = carga_no_asignada*self.costo_oportunidad
+        
+    def _set_costo_total(self):
+        self.costo_total = self.costo_camiones + self.costo_no_asignados
+    
+    def _set_costo_total_tn(self):
+        self.costo_total_tn = round(self.costo_total/self.carga_total, 2)
+        
+    def _set_ahorro(self):
+        self.ahorro = round((self.costo_total_tn - self.presupuesto)/self.presupuesto, 4)
